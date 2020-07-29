8fd96cb61ae9396f1058d7f85eb16b44e3c186f2
Avinash <p4avinashmaurya@gmail.com>
2020-07-29 13:41:22 +0530

    added the initial files and the clock component

---
+++ src/components/clock/clock.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./clock.css";
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    // this.state = { time: Date.now() };
+    this.state = {
+      time: Date.now(),
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(
+      () => this.setState({ time: Date.now() }),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    var d = new Date();
+    var hour = d.getHours();
+    var minutes = d.getMinutes();
+    //var seconds = d.getSeconds();
+
+    return (
+      <div className="clock">
+        {/* <h1>{this.state.time}</h1> */}
+        <h1>{`${hour}:${minutes}`}</h1>
+      </div>
+    );
+  }
+}
+
+export default Clock;
